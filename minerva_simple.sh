a9a6afafc00ce6403d39fc3a19c4ba42094a4b93
jhamer90811 <jhamer90811@gmail.com>
2018-07-31 18:37:24 -0500

    made changes to import calls

--- minerva_simple.sh
+++ minerva_simple.sh
@@ -3,7 +3,7 @@
 #PBS -N minerva_network_topology_analysis
 #PBS -j oe
 #PBS -o /data/jhamer/minerva_output/minerva-simple-log.out
-#PBS -l walltime=00:05:00
+#PBS -l walltime=04:00:00
 #PBS -A minervaG
 #PBS -q amd32
 
@@ -14,7 +14,7 @@ cd /home/jhamer
 export INPUT_PATH='/data/jhamer/minerva_networks/networks'
 export OUTPUT_PATH='/data/jhamer/minerva_output'
 export START_INDEX='1'
-export END_INDEX='50'
+export END_INDEX='5000'
 export MODE='minerva'
 
 singularity exec network_topology.simg python3 MINERvA_NOvA_network_analysis/get_simple_attributes.py ${INPUT_PATH} ${OUTPUT_PATH} ${START_INDEX} ${END_INDEX} ${MODE}
--- nova_simple.sh
+++ nova_simple.sh
@@ -3,7 +3,7 @@
 #PBS -N nova_network_topology_analysis
 #PBS -j oe
 #PBS -o /data/jhamer/nova_output/minerva-simple-log.out
-#PBS -l walltime=00:05:00
+#PBS -l walltime=04:00:00
 #PBS -A minervaG
 #PBS -q amd32
 
@@ -14,7 +14,7 @@ cd /home/jhamer
 export INPUT_PATH='/data/jhamer/nova_networks'
 export OUTPUT_PATH='/data/jhamer/nova_output'
 export START_INDEX='1'
-export END_INDEX='50'
+export END_INDEX='5000'
 export MODE='nova'
 
 singularity exec network_topology.simg python3 MINERvA_NOvA_network_analysis/get_simple_attributes.py ${INPUT_PATH} ${OUTPUT_PATH} ${START_INDEX} ${END_INDEX} ${MODE}
