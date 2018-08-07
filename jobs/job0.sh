f841cb89a2aff574df6dea37b86cf38ba52e3ae1
jhamer90811 <jhamer90811@gmail.com>
2018-08-07 09:06:03 -0500

    made changes to import calls

--- jobs/job0.sh
+++ jobs/job0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -S /bin/bash
-#PBS -N network_topology1
+#PBS -N network_topology0
 #PBS -j oe
 #PBS -o /data/jhamer/minerva_output/
 #PBS -l walltime=24:00:00
@@ -13,9 +13,9 @@ cd /home/jhamer
 
 INPUT_PATH=/data/jhamer/minerva_networks/networks
 OUTPUT_PATH=/data/jhamer/minerva_output/complex_attributes
-IMG_PATH=/lfstev/e-938/jhamer/01/hadmultkineimgs_127x94_me1Amc.hdf5
-START_INDEX=2000
-END_INDEX=2045
+IMG_PATH=/data/jhamer/minerva_imgs/hadmultkineimgs_127x94_me1Amc.hdf5
+START_INDEX=51
+END_INDEX=96
 MODE='minerva'
 
 singularity exec network_topology.simg python3 MINERvA_NOvA_network_analysis/get_complex_attributes.py ${INPUT_PATH} ${OUTPUT_PATH} ${IMG_PATH} ${START_INDEX} ${END_INDEX} ${MODE}
