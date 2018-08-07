f82c66e312ae0aaac3e7a7e78ab6cb8ee62db13f
jhamer90811 <jhamer90811@gmail.com>
2018-08-07 07:47:25 -0500

    made changes to import calls

--- complex_controller.sh
+++ complex_controller.sh
@@ -11,7 +11,13 @@ GEN_PER_BATCH=$5
 ((end = i + GEN_PER_BATCH -1))
 while [ ${i} -le 23 ]
 do
+    if [ ${i} < 10 ]
+    then
     IMGS_PATH=${IMGS_PATH}/0${i}/hadmultkineimgs_127x94_me1Amc.hdf5
+    else
+    IMGS_PATH=${IMGS_PATH}/${i}/hadmultkineimgs_127x94_me1Amc.hdf5
+    fi
+
     cd /home/jhamer/MINERvA_NOvA_network_analysis
     qsub complex_job_submit.sh ${INPUT_PATH} ${OUTPUT_PATH} ${IMGS_PATH} ${start} ${end} ${MODE}
     ((i = i + 1))
